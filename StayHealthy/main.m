4546df4d451924bb6e93eb65e12a411c058e5586
Student <student@MacBook-Pro-0209-RSaunders.local>
2013-12-07 14:04:50 -0500

    Initial Commit

---
+++ StayHealthy/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  StayHealthy
+//
+//  Created by Student on 12/7/2013.
+//  Copyright (c) 2013 Mark Saunders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ StayHealthyTests/StayHealthyTests.m
@@ -0,0 +1,34 @@
+//
+//  StayHealthyTests.m
+//  StayHealthyTests
+//
+//  Created by Student on 12/7/2013.
+//  Copyright (c) 2013 Mark Saunders. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface StayHealthyTests : XCTestCase
+
+@end
+
+@implementation StayHealthyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
